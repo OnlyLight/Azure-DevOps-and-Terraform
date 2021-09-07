ae385adde47a56657c5686a9f037753bd244d04b
Duy Quang Tran <duyquangtran@kms-technology.com>
2021-09-07 10:52:42 +0700

    Edit file azure-pipelines.yml and modify file WeatherForecastController

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace weatherapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "XXX", "XXX", "XXX", "XXX", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
