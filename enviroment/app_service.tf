39a8df29b8432b3525f9fa032df371eae961785a
Duy Quang Tran <duyquangtran@kms-technology.com>
2021-09-17 15:35:59 +0700

    Edit terraform file

---
+++ enviroment/app_service.tf
@@ -0,0 +1,30 @@
+resource "azurerm_app_service_plan" "example" {
+  name                = "service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  # Define Linux as Host OS
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "blazordemo123"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  app_settings = {
+    # Settings for private Container Registires  
+    DOCKER_REGISTRY_SERVER_URL      = "https://hub.docker.com/"
+  }
+
+  site_config {
+    linux_fx_version          = "DOCKER|onlylight291998/docker_dotnet:latest"
+    always_on                 = "true"
+  }
+}
---
+++ enviroment/key_vault.tf
@@ -0,0 +1,39 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kvault" {
+  name                = "kvault-demo-12"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+    ]
+
+    secret_permissions = [
+      "set",
+      "get",
+      "list",
+      "delete",
+      "purge",
+      "recover"
+    ]
+  }
+}
+
+resource "random_string" "password" {
+  length  = 32
+  special = true
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  name         = "secret-sauce"
+  value        = random_string.password.result
+  key_vault_id = azurerm_key_vault.kvault.id
+}
