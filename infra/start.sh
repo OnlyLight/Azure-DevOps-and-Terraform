c01b45c243067410dcfae2ed03b88de2c56335a1
Duy Quang Tran (2782) <duyquangtran@kms.com.vn>
2021-09-27 13:59:54 +0700

    Edit file bash

--- infra/start.sh
+++ infra/start.sh
@@ -4,28 +4,44 @@ if ! command -v az > /dev/null; then
 fi
 
 # Authenticate
-# az login
+az login
 
 # Move to tf
 cd tf/
 
-if grep "environment" env.tfvars; then
-  grep -v "environment" env.tfvars > tmpfile && mv tmpfile env.tfvars
+CMD=$1
+ENV=$2
+
+runTerraform()
+{
+  case $CMD in
+  "init")
+    terraform init
+    ;;
+  "plan")
+    terraform plan -var-file="env.tfvars"
+    ;;
+  "apply")
+    terraform apply -var-file="env.tfvars" -auto-approve
+    ;;
+  "destroy")
+    terraform destroy -var-file="env.tfvars" -auto-approve
+    ;;
+  *)
+    echo "Not Found Command"
+  esac
+}
+
+if [ "$CMD" == "plan" -o "$CMD" == "apply" ]; then
+  if [ $ENV == "staging" -o $ENV == "production" ]; then
+    if grep "environment" env.tfvars; then
+      grep -v "environment" env.tfvars > tmpfile && mv tmpfile env.tfvars
+    fi
+    echo 'environment = "'$ENV'"' >> env.tfvars
+    runTerraform
+  else
+    echo "Please enter environment 'staging' or 'production'"
+  fi
+else
+  runTerraform
 fi
-echo 'environment = "'$2'"' >> env.tfvars
-
-case $1 in
-"init")
-  terraform init
-  ;;
-"plan")
-  terraform plan -var-file="env.tfvars"
-  ;;
-"apply")
-  terraform apply -var-file="env.tfvars" -auto-approve
-  ;;
-"destroy")
-  terraform destroy -var-file="env.tfvars" -auto-approve
-  ;;
-esac
-
